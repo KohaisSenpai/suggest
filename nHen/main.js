bd0296ea8d303a6782c9a08e893ed84d32d2af8a
KohaisSenpai <91554593+KohaisSenpai@users.noreply.github.com>
2021-09-28 22:05:30 +0530

    Update main.js

--- nHen/main.js
+++ nHen/main.js
@@ -9,11 +9,32 @@ const magic = async (d) => {
     console.log(a);
     //b.innerHTML = await getUrl("https://cors-anywhere.herokuapp.com/https://nhentai.net/g/" + a + "/");
     console.log("https://nhentai.net/g/" + a + "/");
+    //b.style.backgroundImage = "url('" + img(await get(proxy(await getUrl(a)))) + "')";
     b.style.backgroundImage = "url('" + (await getUrl(a)) + "')";
+    console.log("url('" + (await getUrl(a)) + "')");
     lock = false;
     //console.log(b);
     //console.log(b.style.backgroundImage);
 };
+
+/*const img = (a) => {
+    data = JSON.parse(a)["contents"];
+    console.log(data);
+    data = new Blob([data]);
+    console.log(data);
+    url = URL.createObjectURL(data);
+    var reader = new FileReader();
+    reader.readAsDataURL(data);
+    reader.onloadend = function () {
+        var base64String = reader.result;
+        console.log("Base64 String - ", base64String);
+
+        // Simply Print the Base64 Encoded String,
+        // without additional data: Attributes.
+        console.log("Base64 String without Tags- ", base64String.substr(base64String.indexOf(", ") + 1));
+    };
+    return url;
+};*/
 
 const proxy = (u) => {
     console.log("https://api.allorigins.win/get?url=" + encodeURIComponent(u));
@@ -40,7 +61,7 @@ const getUrl = async (c) => {
     let cont = (await get(proxy("https://nhentai.net/g/" + c + "/"))).replaceAll("\\", "");
     if (typeof cont == "number" && ("" + c)[0] == 4) return await lop();
     else if (cont == "t") return await getUrl(c);
-    console.log(cont);
+    //console.log(cont);
     cont = cont.substr(cont.indexOf('r.jpg" w') - 69, 96);
     console.log(cont);
     cont = cont.substring(cont.indexOf("http"), cont.indexOf('" w'));
